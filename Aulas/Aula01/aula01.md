ddb5b7c768b19d01689a670329f4d0f405424cb2
Samuel-Junior21 <juniorehigor553@gmail.com>
2024-04-20 12:01:23 -0300

    Adicionando Aula 1 - Módulo 2

---
+++ Aulas/Aula01/aula01.md
@@ -0,0 +1,30 @@
+#  Instalar Plugins
+    - Live server
+    - Material Icon Theme  
+
+## Extensões de arquivos
+    Tudo o que vem depois do "."
+    Exemplo:
+    .html
+    .js
+    .css
+    .cs
+    .c
+    .cpp
+    .java
+    .mp3
+    .mp4 - .mov
+    .pdf
+    .docx
+    .xlsx
+
+## Instalar nodejs
+    -Link download 
+
+## Executar um arquivo em js (javascript) com node
+    - Entrar na pasta do arquivo .js | cd nome-da-pasta
+    - Executar o arquivo .js         | node nome-do-arquivo.js
+    - Limpar tela                    | clear / cls
+    -Listar arquivos no terminal     | ls / dir
+    
+
---
+++ Aulas/Aula01/js/variaveis.js
@@ -0,0 +1,28 @@
+var nome = "Fulano"
+var nome = String("Ciclano")
+
+var numero1 = 5 // number
+var numero2 = 5.5 // number
+var numero3 = Number("10") // number
+var numero4 = Number(100) // number
+var numero5 = parseInt(5.3) // number
+var numero6 = 10 // number
+
+var ehMaiorDeIdade = true // boolean
+var ehVermelho = false // boolean
+
+var naoDefinido = undefined
+var vazio = null
+var naoEhNumero = NaN
+
+console.log(naoDefinido)
+console.log(vazio)
+console.log(naoEhNumero)
+
+console.log(typeof(numero1))
+console.log(typeof(numero2))
+console.log(typeof(numero3))
+console.log(typeof(numero4))
+console.log(typeof(numero5))
+console.log(typeof(numero6))
+
