bc24d4c76bc6aed996d21c583b21650ac2d961dd
Samuel-Junior21 <juniorehigor553@gmail.com>
2024-05-23 14:06:53 -0300

    Lista 5

---
+++ Listas/Lista5/exercicio1/js/ex01.js
@@ -0,0 +1,6 @@
+var nome = prompt("Insira seu nome: ")
+var idade = prompt("Insira sua idade: ")
+var emprego = prompt("Seu emprego atual: ")
+var endereco = prompt("Insira seu endereço: ")
+
+alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nseu emprego atual é: ${emprego} \ne mora no endereço: ${endereco}`)
---
+++ Listas/Lista5/exercicio2/js/ex02.js
@@ -0,0 +1,6 @@
+var nome = "Samuel"
+var idade = 17
+var emprego = "Desenvolvedor FrontEnd"
+var endereco = "Rua Primeiro de Maio, 500"
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nseu emprego atual é: ${emprego} \ne mora no endereço: ${endereco}`)
